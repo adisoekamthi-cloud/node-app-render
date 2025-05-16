cf7b18265fb82e24882bb0f0ea8aeca268befea7
adisoekamthi-cloud <138388038+adisoekamthi-cloud@users.noreply.github.com>
2025-05-16 14:17:08 +0700

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Server Node.js aktif!');
+const { scrapeKuramanime } = require('./scraper'); // Import fungsi dari scraper.js
+
+app.get('/run', async (req, res) => {
+  try {
+    await scrapeKuramanime();
+    res.send('✅ Scraping selesai!');
+  } catch (err) {
+    res.status(500).send('❌ Gagal menjalankan scraping: ' + err.message);
+  }
 });
+
 
 // Gunakan PORT dari environment Railway
 const PORT = process.env.PORT || 3000;
